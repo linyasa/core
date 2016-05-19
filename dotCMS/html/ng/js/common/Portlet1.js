d52d5db7e228c2ace967417ba40b7b4c49c75fb0
Freddy Rodriguez <freddy0309@gmail.com>
2016-05-19 16:22:15 -0430

    Dotcms menu working in angular app

--- dotCMS/html/ng/js/common/Portlet1.js
+++ dotCMS/html/ng/js/common/Portlet1.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", 'angular2/http'], function(exports_1, context_1) {
+System.register(["angular2/core"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,31 +10,24 @@ System.register(["angular2/core", 'angular2/http'], function(exports_1, context_
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1;
+    var core_1;
     var Portlet1;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (http_1_1) {
-                http_1 = http_1_1;
             }],
         execute: function() {
             Portlet1 = (function () {
-                function Portlet1(http) {
-                    var _this = this;
-                    http.get('/api/jwt/poc1').subscribe(function (res) {
-                        _this.poc1Response = res.json();
-                    });
+                function Portlet1() {
                 }
                 Portlet1 = __decorate([
                     core_1.Component({
-                        selector: "ANGULAR_PORTLET3",
-                        template: "\n        <h1>Porlet 1</h1>\n        <br>\n        <h1>This response is coming from a RESTful resource (/api/jwt/poc1): {{poc1Response?.Success}}</h1>\n    ",
-                        providers: [http_1.HTTP_PROVIDERS]
+                        selector: "porlet1",
+                        template: "\n        <h1>Porlet 1</h1>\n    ",
+                        providers: []
                     }), 
-                    __metadata('design:paramtypes', [http_1.Http])
+                    __metadata('design:paramtypes', [])
                 ], Portlet1);
                 return Portlet1;
             }());
--- src/com/dotcms/rest/MenuItem.java
+++ src/com/dotcms/rest/MenuItem.java
@@ -8,11 +8,17 @@ public class MenuItem {
     private String url;
     private String id;
     private String name;
+    private boolean angular = false;
 
     public MenuItem(String id, String url, String name) {
         this.url = url;
         this.id = id;
         this.name = name;
+    }
+
+    public MenuItem(String id, String url, String name, boolean angular) {
+        this(id, url, name);
+        this.angular = angular;
     }
 
     public String getUrl() {
@@ -25,5 +31,9 @@ public class MenuItem {
 
     public String getName() {
         return name;
+    }
+
+    public boolean isAngular() {
+        return angular;
     }
 }
